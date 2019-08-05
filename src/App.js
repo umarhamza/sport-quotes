04af865276b8a6eddc92955543af3592a3ae881e
umar-hamza <umar.hamza@finalsite.com>
2019-08-05 12:34:43 +0100

    updating app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App"> 
+      <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
