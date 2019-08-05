8e030a17f9970b571ef80b011687d38889c946c6
umar-hamza <umar.hamza@finalsite.com>
2019-08-05 12:32:50 +0100

    moving header and footer from index to React components

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = props => {
+
+    return (
+        <footer id="footer" className="footer">
+            <div className="container">
+                <div className="col">
+                    <p className="footer--text">© 2019 Sportlabs Technology Ltd. All rights reserved</p>
+                </div>
+            </div>
+        </footer>
+    );
+
+}
+
+export default Footer;
