880a2a1be2b79fa3e92ded45fe68f616daffe6f9
umar-hamza <umar.hamza@finalsite.com>
2019-08-05 12:32:26 +0100

    moving header and footer from index to React components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = props => {
+
+    return (
+        <header id="header" className="header">
+            <div className="container">
+                <h1 className="header--title">Sporting Quotes</h1>
+            </div>
+        </header>
+    );
+
+}
+
+export default Header;
